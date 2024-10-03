c10688e63208dcd228990ad4795e86bffbbf4f03
MelissaLarangeira <melissalarangeira@gmail.com>
2024-10-03 17:50:28 -0300

    Revert "commit indevido"
    This reverts commit c60a71ef6fe243261436ee572a15096ca00e6578.

---
+++ AecApi/Dao/DatabaseInitializer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+
+namespace AecApi.Dao
+{
+    public class DatabaseInitializer
+    {
+        private readonly MyDbContext _context; // Contexto do Entity Framework
+        private readonly IConfiguration _configuration;
+
+        public DatabaseInitializer(MyDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        public async Task InitializeAsync()
+        {
+            // Verifica se o banco de dados já está criado
+            await _context.Database.EnsureCreatedAsync();
+
+            var scriptName = "Create_Table.sql"; // Nome do script
+            var scriptPath = Path.Combine(Directory.GetCurrentDirectory(), "Dao", scriptName);
+
+       
+        }
+    }
+
+}
